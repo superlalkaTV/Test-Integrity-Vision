4eb99b0e82ebbe290119237789c276abe0058219
Maksym Bataiev <max.bataiev@gmail.com>
2021-07-10 22:43:37 +0300

    Initial commit

---
+++ src/main/java/ua/maksym/bataiev/testintegrityvision/TestIntegrityVisionApplication.java
@@ -0,0 +1,13 @@
+package ua.maksym.bataiev.testintegrityvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIntegrityVisionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestIntegrityVisionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ua/maksym/bataiev/testintegrityvision/TestIntegrityVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.maksym.bataiev.testintegrityvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIntegrityVisionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
